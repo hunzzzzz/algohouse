e941963d436feffb0892b2cacdbda67bcf91c0ef
hunzz <hunzz.study@gmail.com>
2024-07-27 11:49:03 +0900

    [실버] 1927번 문제 풀이 수정

--- src/main/kotlin/silver/q1927/Solution.kt
+++ src/main/kotlin/silver/q1927/Solution.kt
@@ -1,24 +1,30 @@
 package silver.q1927
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
+import java.io.StreamTokenizer
 import java.util.*
 
 fun main() {
-    val reader = BufferedReader(InputStreamReader(System.`in`))
-    val writer = BufferedWriter(OutputStreamWriter(System.out))
-    val queue = PriorityQueue<Int>() // 우선순위 큐
+    StreamTokenizer(System.`in`.bufferedReader()).run {
+        fun int() = nextToken().run { nval.toInt() } // 정수를 입력받는 함수
 
-    repeat(reader.readLine().toInt()) {
-        when (val input = reader.readLine().toInt()) {
-            // 명령어(input)에 따른 동작
-            0 -> writer.write("${if (queue.isEmpty()) 0 else queue.poll()}\n")
-            else -> queue.offer(input)
+        val queue = PriorityQueue<Int>() // 우선순위 큐
+
+        // 문자열 생성
+        buildString {
+            // 첫 번째 입력값(연산 개수)만큼 반복문 수행
+            repeat(int()) {
+                when (val input = int()) {
+                    // 명령어에 따른 수행
+                    0 -> append("${if (queue.isEmpty()) 0 else queue.poll()}\n")
+                    else -> queue.offer(input)
+                }
+            }
+        }.let {
+            // 문자열 출력
+            System.out.bufferedWriter().use { writer ->
+                writer.write(it)
+                writer.flush()
+            }
         }
     }
-
-    reader.close()
-    writer.close()
 }
